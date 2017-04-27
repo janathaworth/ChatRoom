8895ee46d6339d6ec05570f94dfddd25ddbc9fca
janat haworth <janathaworth@wireless-10-145-81-68.public.utexas.edu>
2017-04-27 15:17:17 -0500

    online users working

--- Client.java
+++ Client.java
@@ -5,13 +5,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -19,6 +29,7 @@ import javafx.stage.Stage;
 public class Client  {
 	int port = 4242;
 	
+	ListView<String> list;
 	private BufferedReader reader; 
 	private PrintWriter writer;
 	TextArea ta;
@@ -28,33 +39,54 @@ public class Client  {
 	
 	public Client(String name, Stage s) {
 		this.name = name; 
-		//Stage s = new Stage(); 
+		s = new Stage(); 
 		Scene scene = getScene(); 
 		s.setTitle(name); // Set the stage title
 		s.setScene(scene); // Place the scene in the stage
 		s.show(); // Display the stage
+		writer.println("tparsemet" + name);
+		writer.flush();
 	}
 	
 	public Scene getScene() {
 		// Panel p to hold the label and text field
 		// Create UI
+		BorderPane border = new BorderPane(); 
 		FlowPane pane = new FlowPane();
 		FlowPane pane1 = new FlowPane();
 		pane1.setHgap(4);
-		pane1.setStyle("-fx-border-color: green");
+		//pane1.setStyle("-fx-border-color: green");
 		pane1.setAlignment(Pos.CENTER);
 		tf = new TextField();
 		VBox v = new VBox(); 
 		ta = new TextArea();
+		ta.setPrefRowCount(23);
 		ta.setEditable(false);
-		v.getChildren().addAll(new ScrollPane(ta), pane1);
-		VBox online = new VBox(); 
+		TextArea clients = new TextArea(); 
+		clients.setEditable(false);
+		clients.setPrefColumnCount(14);
+		//ScrollPane online = new ScrollPane(clients);
+		
+		MenuBar menuBar = new MenuBar(); 
+		ScrollPane online = new ScrollPane();
+		
+		 list = new ListView<>();
+
+		ObservableList<String> items = FXCollections.observableArrayList();
+		items.add("Online Users:");
 		for (String name : Server.names.keySet()) {
-			Button button = new Button(name);
-			button.setStyle("-fx-background-color: transparent;");
-			online.getChildren().add(button);
+			if (!name.equals(this.name)){
+				items.add(name);
+				Menu menu1 = new Menu(name);
+				menuBar.getMenus().add(menu1);
+				clients.appendText(name + " is online\n");
+			}
 		}
-		pane.getChildren().addAll(online, v);
+		list.setItems(items);
+		online.setContent(menuBar);
+		border.setLeft(list);
+		//border.setBottom(pane1);
+		pane.getChildren().add( v);
 		// handle action event
 		tf.setOnAction(e -> {
 			// get the message from the text field
@@ -65,6 +97,7 @@ public class Client  {
 			writer.flush();
 		});
 		Button sendBt = new Button("Send");
+		sendBt.setStyle("-fx-background-color: #55ffff");
 		sendBt.setOnAction(e -> {
 			// get the message from the text field
 			String message = tf.getText();
@@ -73,10 +106,17 @@ public class Client  {
 			//System.out.println(message);
 			writer.flush(); 
 		});
+		
+		
+		//String image = "http://allenage.com/wp-content/uploads/2016/03/crying-emoji.jpg?15b286"; 
+		//Button emoji = new Button();
+		//emoji.setGraphic(new ImageView(image));
 		pane1.getChildren().addAll(new Label("Enter a message: "), tf, sendBt);
+		v.getChildren().addAll(new ScrollPane(ta), pane1);
+		border.setRight(pane);
 		Socket sock;
 		try {
-			sock = new Socket("10.146.144.25", 4242);
+			sock = new Socket("127.0.0.1", 4242);
 			writer = new PrintWriter(sock.getOutputStream());
 			InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
 			reader = new BufferedReader(streamReader);
@@ -86,17 +126,34 @@ public class Client  {
 		catch (Exception e) {
 					e.printStackTrace(); 
 		}
-		return new Scene(pane);
+		return new Scene(border);
 	}
 		
 	class IncomingReader implements Runnable {
+		String message; 
 		public void run() { 
 			try {
-				String message = reader.readLine(); 
+				
+				message = reader.readLine(); 
+				String check = "tparsemet" + name;
 				while (message != null) {
-					ta.appendText(message + "\n");
+					//System.out.println(message);
+					if (!message.equals(check)) {
+						if (message.contains("tparsemet")) {
+							message = message.substring(9);
+							Platform.runLater(new Runnable() {
+				                 @Override public void run() {
+				                	
+				                	 list.getItems().add(message);
+				                 }
+							});
+						}
+						else {
+							ta.appendText(message + "\n");
+						}
+					}
 					message = reader.readLine(); 
-				}	
+				}
 			} 
 			catch (IOException ex) { ex.printStackTrace(); }
 			}
--- ClientMain.java
+++ ClientMain.java
@@ -3,6 +3,7 @@ package assignment7;
 import java.util.HashMap;
 import java.util.Map;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,6 +15,8 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 public class ClientMain extends Application {
     
@@ -35,28 +38,108 @@ public class ClientMain extends Application {
 		
 		Button login = new Button("Log In");
 		Hyperlink register = new Hyperlink("Create Account");
-		box.getChildren().addAll(user, pass, login, register);
+		Label incorrect = new Label();
+		box.getChildren().addAll(user, pass, login, register, incorrect);
 		box.setAlignment(Pos.CENTER);
+		box.setSpacing(10);
+		VBox.setMargin(user, new Insets(20, 0, 0, 0));
+		VBox.setMargin(login, new Insets(0, 0, 0, 40));
+		VBox.setMargin(register, new Insets(0, 0, 0, 42));
+		VBox.setMargin(incorrect, new Insets(0, 0, 0, 42));
 		Scene scene = new Scene(box);
 		primaryStage.setScene(scene);
-		Label incorrect = new Label();
-		login.setOnAction( e -> {
-			Client client = new Client(name.getText(), primaryStage);
-             synchronized (Server.names) {
-                 if (Server.names.containsKey(name.getText())) {			//username exists
-                	 if(Server.names.get(name.getText()) != pw.getText()) {	//password does not match
-                		 incorrect.setText("Your password is incorrect!");
-                         incorrect.setTextFill(Color.rgb(210, 39, 30));
-                         VBox iBox = new VBox(); 
-                         iBox.getChildren().add(incorrect);
-                	 }
-                 }
-                 else{
-                	 Server.names.put(name.getText(), pw.getText());	//add new user
-                 }
-             }
-			name.clear();
+		
+		EventHandler log = new EventHandler<ActionEvent> () {
+			@Override public void handle(ActionEvent e) {
+				synchronized (Server.names) {
+	                 if (Server.names.containsKey(name.getText())) {			//username exists
+	                	 if(Server.names.get(name.getText()) != pw.getText()) {	//password does not match
+	                		 incorrect.setText("Your password is incorrect!");
+	                         incorrect.setTextFill(Color.rgb(210, 39, 30));
+	                	 }
+	                	 else {
+	                		 Client client = new Client(name.getText(), primaryStage);
+	                	 }
+	                 }
+	                 else{
+	                	 incorrect.setText("Incorrect Username!");
+	                	 incorrect.setTextFill(Color.rgb(210, 39, 30));
+	                 }
+	             }
+	             
+				name.clear();
+				pw.clear(); 
+			}
+		};
+		
+		login.setOnAction(log);
+		pw.setOnAction(log);
+		
+		
+		register.setOnAction( e -> {
+			
+			VBox box2 = new VBox(); 
+			FlowPane user2 = new FlowPane();
+			FlowPane pass2 = new FlowPane(); 
+			FlowPane pass3 = new FlowPane(); 
+			TextField name2 = new TextField();
+			PasswordField pw2 = new PasswordField();
+			PasswordField pw3 = new PasswordField();
+			user2.getChildren().addAll(new Label("Username: "), name2);
+			pass2.getChildren().addAll(new Label("Password:  "), pw2 );
+			pass3.getChildren().addAll(new Label("Re-Enter Password: "), pw3 );
+			user2.setAlignment(Pos.CENTER);
+			pass2.setAlignment(Pos.CENTER);
+			//pass3.setAlignment(Pos.CENTER);
+			Button reg = new Button("Register");
+			Label incorrect2 = new Label();
+			box2.getChildren().addAll(user2, pass2, pass3, reg, incorrect2);
+			box2.setAlignment(Pos.CENTER);
+			box2.setSpacing(10);
+			VBox.setMargin(user2, new Insets(20, 0, 0, 0));
+			VBox.setMargin(reg, new Insets(0, 0, 0, 50));
+			VBox.setMargin(incorrect2, new Insets(0, 0, 0, 46));
+			VBox.setMargin(pass3, new Insets(0, 0, 0, 54));
+			
+			Scene newScene = new Scene(box2); 
+			primaryStage.setScene(newScene);
+			
+			EventHandler click = new EventHandler<ActionEvent> () {
+				@Override public void handle(ActionEvent e) {
+					synchronized (Server.names) {
+		                 if (Server.names.containsKey(name2.getText())) {			
+		                		incorrect2.setText("Username already taken!");
+		                        incorrect2.setTextFill(Color.rgb(210, 39, 30));
+		                 }
+		                 else if (name2.getText().equals("")) {
+		                	 incorrect2.setText("Please enter a username");
+	                         incorrect2.setTextFill(Color.rgb(210, 39, 30));
+		                 }
+		                 else if (pw2.getText().equals("")) {
+		                	 incorrect2.setText("Please enter a password");
+	                         incorrect2.setTextFill(Color.rgb(210, 39, 30));
+		                 }
+		                 else if(!pw2.getText().equals(pw3.getText())) {
+		                	 incorrect2.setText("Passwords do not match. Please try again.");
+	                         incorrect2.setTextFill(Color.rgb(210, 39, 30));
+		                 }
+		                 else{
+		                	 Server.names.put(name2.getText(), pw2.getText());	//add new user
+		                	 Client client = new Client(name2.getText(), primaryStage);
+		                 }
+		             }
+		             
+					name2.clear();
+					pw2.clear(); 
+					pw3.clear(); 
+	            }
+			};
+			
+			reg.setOnAction(click);
+			pw3.setOnAction(click);
+
 		});
+		
 		
 		primaryStage.show();
 	}
