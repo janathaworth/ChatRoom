9a0576442aa0ad0275d45ac29e6f50748d0a8719
janat haworth <janathaworth@wireless-10-146-204-23.public.utexas.edu>
2017-04-28 12:08:24 -0500

    IP address

--- Client.java
+++ Client.java
@@ -70,7 +70,7 @@ public class Client  {
 	public Scene getScene() {
 		
 		try {
-			Socket sock = new Socket("192.168.1.116", 4242);
+			Socket sock = new Socket("10.146.204.23", 4242);
 			writer = new PrintWriter(sock.getOutputStream());
 			InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
 			reader = new BufferedReader(streamReader);
